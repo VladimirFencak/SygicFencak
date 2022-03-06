63525c91aedc32012e3eb9ef374764288bce17da
kemperik <vladimir.fencak@gmail.com>
2022-03-06 13:07:27 +0100

    Google maps integration

--- app/src/main/java/com/example/sygicfencak/MainActivity.kt
+++ app/src/main/java/com/example/sygicfencak/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -21,13 +22,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SygicFencakTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    MapScreen()
-                }
+                MapScreen()
             }
         }
     }
--- app/src/main/java/com/example/sygicfencak/presentation/map/MapScreen.kt
+++ app/src/main/java/com/example/sygicfencak/presentation/map/MapScreen.kt
@@ -2,18 +2,45 @@ package com.example.sygicfencak.presentation.map
 
 
 import android.Manifest
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import com.example.sygicfencak.utils.FeatureWhichRequirePermission
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @ExperimentalPermissionsApi
 @Composable
 fun MapScreen(){
-    FeatureWhichRequirePermission(
-        permissions = listOf(Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION),
-        basicDialogText = "Training tracking requires location permission",
-        rationaleDialogText = "We cannot track your training without knowing your location",
-        onlySomePermissionGrantedText = "Thank you for access to your approximate location "+
-                "but we need precise location for training tracking")
+    Box(Modifier.fillMaxSize()) {
+        val singapore = LatLng(1.35, 103.87)
+        val cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(singapore, 10f)
+        }
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState
+        ) {
+            Marker(
+                position = singapore,
+                title = "Singapore",
+                snippet = "Marker in Singapore"
+            )
+        }
+        FeatureWhichRequirePermission(
+            permissions = listOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ),
+            basicDialogText = "Training tracking requires location permission",
+            rationaleDialogText = "We cannot track your training without knowing your location",
+            onlySomePermissionGrantedText = "Thank you for access to your approximate location " +
+                    "but we need precise location for training tracking"
+        )
+    }
 }
